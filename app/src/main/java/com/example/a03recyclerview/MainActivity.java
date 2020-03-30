a00f93900672db8400ea11cc99b46238c344cc9c
geanclv <geanclv@gmail.com>
2020-03-29 19:59:15 -0500

    Terminando de implementar el primer RecyclerView, con el ViewHolder y el OnItemClick que los complementan.

--- app/src/main/java/com/example/a03recyclerview/MainActivity.java
+++ app/src/main/java/com/example/a03recyclerview/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.a03recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> lstNombre;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initControls();
+        initLstNombre();
+
+        //Iniciando el layoutManager
+        layoutManager = new LinearLayoutManager(this);
+        //Iniciando el adaptador
+        adapter = new MyAdapter(lstNombre, R.layout.recycler_view_item,
+                new MyAdapter.OnItemClickListener() {
+            //Esta es nuestra interfaz, que ahora estamos implementando
+            @Override
+            public void onItemClick(String nombre, int posicion) {
+                Toast.makeText(MainActivity.this, nombre + " " + posicion,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        //Asignando el adaptador y el layoutManager al recyclerView
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initLstNombre () {
+        lstNombre = new ArrayList<String>(){{
+            add("Gean");
+            add("Carlo");
+            add("Pepe");
+            add("Jose");
+            add("Luis");
+        }};
+    }
+
+    private void initControls () {
+        recyclerView = findViewById(R.id.recyclerViewMain);
     }
 }
--- app/src/main/java/com/example/a03recyclerview/MyAdapter.java
+++ app/src/main/java/com/example/a03recyclerview/MyAdapter.java
@@ -1,5 +1,6 @@
 package com.example.a03recyclerview;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,32 +14,63 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     private List<String> lstNombre;
     private int layout;
-    private OnItemClickListener listener;
+    private OnItemClickListener itemClickListener;
+
+    public MyAdapter (List<String> lstNombre, int layout, OnItemClickListener itemClickListener) {
+        this.lstNombre = lstNombre;
+        this.layout = layout;
+        this.itemClickListener = itemClickListener;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //Inflando la vista con el contexto del padre y el layout que tenemos como input
+        View v = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+        //Inicializamos el ViewHolder y le pasamos la vista
+        ViewHolder vh = new ViewHolder(v);
+
+        return vh;
     }
 
+    //Método que se ejecuta al construir el RecyclerView, no se actualiza si se llenan más elementos
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        //Llamamos a nuestro método bind
+        holder.bind(lstNombre.get(position), itemClickListener);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstNombre.size();
     }
 
     //1. ViewHolder para el Adaptador de RecyclerView
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView nombre;
+        public TextView textViewNombre;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.nombre = itemView.findViewById(R.id.textViewNombre);
+            this.textViewNombre = itemView.findViewById(R.id.textViewNombre);
+        }
+
+        //Método para llenar los datos a nuestra vista, como parámetros tiene:
+        // 1. Nuestro modelo (en este caso un string, puede ser un objeto)
+        // 2. Nuestro listener (onItemClickListener)
+        public void bind (final String nombre, final OnItemClickListener listener) {
+            //Llenando la vista
+            this.textViewNombre.setText(nombre);
+            //Implementamos el click
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Llamamos al método que hemos definido en nuestra interface, recibe como parámetro:
+                    // 1. El valor que estamos trabajando (ahora un string, puede ser un objeto)
+                    // 2. La posición
+                    listener.onItemClick(nombre, getAdapterPosition());
+                }
+            });
         }
     }
 
